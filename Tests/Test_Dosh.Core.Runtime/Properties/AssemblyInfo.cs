3e0fef2798f2a3d8ba510cb7bdad6b1f67da4b81
EaE_2626 <imposter.kore2626@gmail.com>
2020-02-19 12:15:40 +0900

    [add] Dosh.Core.Runtime用の単体テストを追加。

---
+++ Tests/Test_Dosh.Core.Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test_Dosh.Core.Runtime")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Test_Dosh.Core.Runtime")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d1eec2c-7106-460e-bad4-33ea28a26c4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
