cab3bff4cd65b2c1cd7a80fc738f2989738f0748
EaE_2626 <imposter.kore2626@gmail.com>
2020-10-19 13:32:18 +0900

    Added YamlDotNet library.

---
+++ Dosh.Core.Runtime/Core/Parser/Base/IParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dosh.Core.Parser
+{
+    /// <summary>
+    /// Parser interface
+    /// </summary>
+    public interface IParser<T>
+    {
+        T Parse();
+    }
+}
---
+++ Dosh.Core.Runtime/Core/Parser/DoshParser.cs
@@ -0,0 +1,16 @@
+﻿using Dosh.Core.DoshFile;
+using System;
+
+namespace Dosh.Core.Parser
+{
+    /// <summary>
+    /// DoshFile parser
+    /// </summary>
+    public class DoshParser : IParser<DoshFileModel>
+    {
+        public DoshFileModel Parse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
