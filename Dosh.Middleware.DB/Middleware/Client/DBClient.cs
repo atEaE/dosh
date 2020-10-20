55d3b81b10cbc009f3c468f783ee82a35d6949aa
VINCI-PC1023\daiki.takahashi <daiki.takahashi@i-Vinci.co.jp>
2020-10-20 12:05:44 +0900

    Implemented the minimum functionality of DBClient and create-connectionstring process.

---
+++ Dosh.Middleware.DB/Middleware/Client/DBClient.cs
@@ -0,0 +1,119 @@
+﻿using Dosh.Core.Logger;
+using Dosh.Middleware.DB.Middleware.Base;
+using Serilog.Events;
+using System;
+using System.Data;
+using System.Data.Common;
+
+namespace Dosh.Middleware.DB.Middleware.Client
+{
+    /// <summary>
+    /// DBClient Class.
+    /// </summary>
+    public class DBClient : IDBClient
+    {
+        ILogger Logger;
+
+        public DBClient(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        /// <summary>
+        /// Create dbConnection
+        /// </summary>
+        /// <param name="providerName">propvider name</param>
+        /// <param name="connectionString">connection string</param>
+        /// <returns>DbConnection Class. if the connection were not made, return null</returns>
+        public DbConnection CreateDbConnection(string providerName, string connectionString)
+        {
+            DbConnection connection = null;
+
+            if (connectionString != null)
+            {
+                try
+                {
+                    var factory = DbProviderFactories.GetFactory(providerName);
+
+                    connection = factory.CreateConnection();
+                    connection.ConnectionString = connectionString;
+                }
+                catch (Exception ex)
+                {
+                    if (connection != null)
+                    {
+                        connection = null;
+                    }
+                    Logger.OutputLog(LogEventLevel.Error, ex.Message);
+                }
+            }
+
+            return connection;
+        }
+
+        /// <summary>
+        /// Implement select query
+        /// <summary>
+        /// <param name="connection">Db connection</param>
+        /// <param name="queryString">query string</param>
+        public void DbCommandSelect(DbConnection connection, string queryString)
+        {
+            if (connection == null)
+            {
+                throw new Exception();
+            }
+
+            using (connection)
+            {
+                try
+                {
+                    DbCommand command = connection.CreateCommand();
+                    command.CommandText = queryString;
+                    command.CommandType = CommandType.Text;
+
+                    connection.Open();
+
+                    DbDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.OutputLog(LogEventLevel.Error, ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Implement query
+        /// </summary>
+        /// <param name="connection">Db connection</param>
+        /// <param name="queryString">query string</param>
+        public void ExecuteDbCommand(DbConnection connection, string queryString)
+        {
+            if (connection == null)
+            {
+                throw new Exception();
+            }
+
+            using (connection)
+            {
+                try
+                {
+                    DbCommand command = connection.CreateCommand();
+                    command.CommandText = queryString;
+
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Logger.OutputLog(LogEventLevel.Error, ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Dosh.Middleware.DB/Middleware/Client/ProviderName.cs
@@ -0,0 +1,7 @@
+﻿namespace Dosh.Middleware.DB.Middleware.Client
+{
+    public readonly struct ProviderName
+    {
+        public static readonly string SqlServer = "System.Data.SqlClient";
+    }
+}
